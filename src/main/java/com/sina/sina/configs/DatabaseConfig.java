326d47cfc9a4b068a129f41b0372a2bf3228e683
mohsen <alimohammadi340@gmail.com>
2017-08-25 01:08:26 +0430

    Hibernate configs deleted

--- src/main/java/com/sina/sina/configs/DatabaseConfig.java
+++ src/main/java/com/sina/sina/configs/DatabaseConfig.java
@@ -7,13 +7,10 @@ package com.sina.sina.configs;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import java.util.Properties;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -36,18 +33,6 @@ public class DatabaseConfig {
     @Value("${db.username}")
     private String DB_USERNAME;
 
-    @Value("${hibernate.dialect}")
-    private String HIBERNATE_DIALECT;
-
-    @Value("${hibernate.show_sql}")
-    private String HIBERNATE_SHOW_SQL;
-
-    @Value("${hibernate.hbm2ddl.auto}")
-    private String HIBERNATE_HBM2DDL_AUTO;
-
-    @Value("${entitymanager.packagesToScan}")
-    private String ENTITYMANAGER_PACKAGES_TO_SCAN;
-
     @Bean
     public DataSource dataSource() {
         HikariConfig config = new HikariConfig();
@@ -59,27 +44,4 @@ public class DatabaseConfig {
         config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
         return new HikariDataSource(config);
     }
-
-    @Bean
-    public LocalSessionFactoryBean sessionFactory() {
-        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
-        sessionFactoryBean.setDataSource(dataSource());
-        sessionFactoryBean.setPackagesToScan(ENTITYMANAGER_PACKAGES_TO_SCAN);
-        Properties hibernateProperties = new Properties();
-        hibernateProperties.put("hibernate.dialect", HIBERNATE_DIALECT);
-        hibernateProperties.put("hibernate.show_sql", HIBERNATE_SHOW_SQL);
-        hibernateProperties.put("hibernate.hbm2ddl.auto", HIBERNATE_HBM2DDL_AUTO);
-        sessionFactoryBean.setHibernateProperties(hibernateProperties);
-
-        return sessionFactoryBean;
-    }
-
-    @Bean
-    public HibernateTransactionManager transactionManager() {
-        HibernateTransactionManager transactionManager
-                = new HibernateTransactionManager();
-        transactionManager.setSessionFactory(sessionFactory().getObject());
-        return transactionManager;
-    }
-
 }
